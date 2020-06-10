0643f88bb0a44db898307d5d1e3d17d72fd72c78
rubin-gower <rubinsemails@gmail.com>
2020-06-11 11:13:59 +1200

    added ability for users to jain and controll own char

---
+++ gamefiles/network/Assets/playerscr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class playerscr : NetworkBehaviour
+{
+
+
+    [SerializeField] private Vector3 movment = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    [Client]
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.W) && hasAuthority)
+        {
+            transform.Translate(movment); 
+        }
+    }
+}
